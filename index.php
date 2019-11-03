7a89319212d58b876fc02b0f4628e29a3194d97b
Bitcoinera <anagutjor@gmail.com>
2019-11-03 14:07:35 +0100

    try to implement consistency. type error

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "
+http://www.w3.org/TR/html4/loose.dtd">
+<!-- TODO: add name, number of task, game's name... -->
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <title>Rock - Paper - Scissors</title>
+    <title>Mastermind</title>
     <link rel="stylesheet" href="style.css" type="text/css" media="screen">
 </head>
 
@@ -16,13 +18,13 @@
 
     function gen_rand_number() {
         $GLOBALS['rand_number'] = rand(1000,9999);
-        echo "Numero: " . $GLOBALS['rand_number'];
+        echo 'Numero: ' . $GLOBALS['rand_number'];
     }
 
     gen_rand_number();
     ?>
 
-    <form action="#" method="post">
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
     <?php
 
     function debug_to_console($data) {
@@ -30,23 +32,25 @@
         if (is_array($output))
             $output = implode(',', $output);
 
-        echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+        echo '<script>console.log("Debug Objects: ' . $output . '" );</script>';
     }
 
-    $message = "";
+    $message = '';
     if (isset($_POST['submitButton'])){ //check if form was submitted
-        $num1 = $_POST['num1']; 
-        $num2 = $_POST['num2']; 
-        $num3 = $_POST['num3'];
-        $num4 = $_POST['num4'];
-        $message = "Success! You entered: ". $num1 . $num2 . $num3 . $num4;
+        $num1 = strval($_POST['num1']); 
+        $num2 = strval($_POST['num2']); 
+        $num3 = strval($_POST['num3']);
+        $num4 = strval($_POST['num4']);
+        $plays_arr = $_POST['plays'];
+        $message = 'Success! You entered: '. $num1 . $num2 . $num3 . $num4;
         echo $message;
-        echo "<hr>";
+        echo '<hr>';
         $rand_num_str = strval($GLOBALS['rand_number']);
 
         $num_arr = array($num1, $num2, $num3, $num4);
         $hurts = 0; // number of 'heridos'
         $deads = 0; // number of 'muertos'
+        // $plays_arr = array(); // array to keep track of plays
         $rand_num_arr = str_split($rand_num_str);
 
         foreach ($num_arr as &$value) {
@@ -58,13 +62,17 @@
                 if ($num_arr[$key] === $value) {
                     $deads++;
                 }
-            } 
+            }
         }
 
-        echo "Heridos hay " . $hurts . "<br>";
-        echo "Muertos hay " . $deads . "<br>";
+        echo '<p>Jugada tal: hay ' . $hurts . ' heridos y ' . $deads . ' muertos.</p>';
+        debug_to_console($plays_arr);
+        $plays_arr[] = $hurts;
     }
     ?>
+        <!-- here we will display the results of each play in order -->
+        <input type="text" name="plays" value="<?php if (isset($plays_arr)) echo implode("," , $plays_arr) ?>" style="text-align:right;"/><br>
+
         <label for="num1">Introduce el número</label>
         <input type="number" name="num1" size="1" maxlength="1" min="0" max="9" value="0">
         <input type="number" name="num2" size="1" maxlength="1" min="0" max="9" value="0">
